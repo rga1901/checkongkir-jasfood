6a1108b1e433331c03c5aa2a914ce455632b6461
rga1901 <rgayoga@gmail.com>
2026-09-19 18:51:03 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@
 // Versi gak lagi dinamain v1, v2, v3, dst. Tiap ada update, ganti nama versi
 // pakai nama gunung yang ada di Indonesia, bebas gunung mana aja, TAPI harus
 // beda dari nama gunung yang dipakai di versi sebelumnya. Format bebas asal
-// jelas, contoh: 'cek-ongkir-merbabu', 'cek-ongkir-semeru', dst.
-const CACHE_VERSION = 'cek-ongkir-merbabu';
+// jelas, contoh: 'cek-ongkir-lawu', 'cek-ongkir-semeru', dst.
+const CACHE_VERSION = 'cek-ongkir-lawu';
 const FILES_TO_CACHE = [
   './',
   './index.html',
